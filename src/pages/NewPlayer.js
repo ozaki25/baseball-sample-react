09050f053de1db367f80e26ff9be937b44d493af
ozaki <fs65ugumori@yahoo.co.jp>
2019-08-15 00:49:20 +0900

    update new player

--- src/pages/NewPlayer.js
+++ src/pages/NewPlayer.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { Panel } from 'react-bootstrap-component';
+import React, { useState } from 'react';
+import { Panel, Form } from 'react-bootstrap-component';
+import useReactRouter from 'use-react-router';
+import usePlayer from '../hooks/usePlayer';
+import { Routes } from '../router';
 
 function NewPlayer() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [team, setTeam] = useState('');
+  const [position, setPosition] = useState('');
+
+  const { history } = useReactRouter();
+  const { addPlayer } = usePlayer();
+
+  const items = [
+    { label: '名前', name: 'name', onChange: e => setName(e.target.value) },
+    { label: '年齢', name: 'age', onChange: e => setAge(e.target.value) },
+    { label: 'チーム', name: 'team', onChange: e => setTeam(e.target.value) },
+    {
+      label: '守備位置',
+      name: 'position',
+      onChange: e => setPosition(e.target.value),
+    },
+  ];
+
+  const onSubmit = () => {
+    const player = {
+      name,
+      age,
+      team,
+      position,
+    };
+    addPlayer(player);
+    history.push(Routes.playerList);
+  };
+
   return (
     <Panel title="新規作成">
-      <h1>New</h1>
+      <Form
+        items={items}
+        buttonProps={{ label: '作成', primary: true }}
+        onSubmit={onSubmit}
+      />
     </Panel>
   );
 }
